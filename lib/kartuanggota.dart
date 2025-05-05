29b4613f1fe198a5071f6d7a6b60db6800d66d7f
Raafi Suwargana Putra <rafisuwarganaptr@gmail.com>
2025-05-05 22:35:41 +0700

    fix

--- lib/kartuanggota.dart
+++ lib/kartuanggota.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'dart:math';
 
 class KartuanggotaPage extends StatelessWidget {
-  const KartuanggotaPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    double cardWidth = screenWidth * 0.9;
-    double cardHeight =
-        cardWidth * 0.6; // Menyesuaikan rasio kartu agar tetap proporsional
+    double cardWidth = screenWidth * 1.1;
+    double cardHeight = cardWidth * 0.8;
 
     return Scaffold(
       backgroundColor: Colors.white,
@@ -23,66 +21,69 @@ class KartuanggotaPage extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: Container(
-          width: cardWidth,
-          height: cardHeight,
-          padding: EdgeInsets.all(20),
-          decoration: BoxDecoration(
-            color: Color(0xFF78B7EB),
-            borderRadius: BorderRadius.circular(20),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black26,
-                blurRadius: 5,
-                offset: Offset(3, 3),
-              ),
-            ],
-          ),
-          child: Row(
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    radius: 40,
-                    backgroundImage:
-                        Image.asset('assets/images/profile/Profile.png').image,
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    "ID: 000123",
-                    style: GoogleFonts.poppins(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 14,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(width: 15),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+        child: Transform.rotate(
+          angle: pi / 2,
+          child: Container(
+            width: cardWidth,
+            height: cardHeight,
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Color(0xFF78B7EB),
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 5,
+                  offset: Offset(3, 3),
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundImage:
+                          Image.asset('assets/images/profile/profile.png').image,
+                    ),
+                    SizedBox(height: 10),
                     Text(
-                      "KARTU ANGGOTA PERPUSTAKAAN",
+                      "ID: 000123",
                       style: GoogleFonts.poppins(
-                        fontWeight: FontWeight.w500,
+                        fontWeight: FontWeight.bold,
                         color: Colors.white,
-                        fontSize: 12,
+                        fontSize: 14,
                       ),
                     ),
-                    SizedBox(height: 6),
-                    _buildInfoRow(
-                        "Nama Lengkap", "Asep Kurniawan", Color(0xFF3E78B2)),
-                    _buildInfoRow(
-                        "Tanggal Bergabung", "25/12/2025", Color(0xFF3E78B2)),
-                    _buildInfoRow("Status", "AKTIF", Color(0xFF3AA86D)),
                   ],
                 ),
-              ),
-            ],
+                SizedBox(width: 15),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "KARTU ANGGOTA PERPUSTAKAAN",
+                        style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                          fontSize: 12,
+                        ),
+                      ),
+                      SizedBox(height: 6),
+                      _buildInfoRow(
+                          "Nama Lengkap", "Asep Kurniawan", Color(0xFF3E78B2)),
+                      _buildInfoRow(
+                          "Tanggal Bergabung", "25/12/2025", Color(0xFF3E78B2)),
+                      _buildInfoRow("Status", "AKTIF", Color(0xFF3AA86D)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
